e2c06547fe62cbfa8d615e9b841b1ddcf3e2f85f
SangeethS123 <71600115+SangeethS123@users.noreply.github.com>
2024-12-26 23:08:00 +0530

    Updated handler Classes

--- Discord Bot/Handlers/command_handler.js
+++ Discord Bot/Handlers/command_handler.js
@@ -1,14 +1,27 @@
 const fs = require('fs');
 
-module.exports = (client, Discord) => {
+module.exports = (client) => {
     const command_files = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
-    for(const file of command_files){
-        const command = require(`../commands/${file}`);
-        if(command.name){
-            client.commands.set(command.name, command)
-        }else{
-            continue;
+    for (const file of command_files) {
+        try {
+            const command = require(`../commands/${file}`);
+
+            // Validate command structure
+            if (command.name && typeof command.execute === 'function') {
+                client.commands.set(command.name, command);
+
+                // Register aliases, if any
+                if (command.aliases && Array.isArray(command.aliases)) {
+                    command.aliases.forEach(alias => {
+                        client.commands.set(alias, command);
+                    });
+                }
+            } else {
+                console.warn(`Command "${file}" is missing 'name' or 'execute' function.`);
+            }
+        } catch (error) {
+            console.error(`Error loading command file "${file}":`, error);
         }
-    } 
-}
+    }
+};
--- Discord Bot/Handlers/event_handler.js
+++ Discord Bot/Handlers/event_handler.js
@@ -5,11 +5,22 @@ module.exports = (client, Discord) => {
         const event_files = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
 
         for (const file of event_files) {
-            const event = require(`../events/${dirs}/${file}`);
-            const event_name = file.split('.')[0];
-            client.on(event_name, event.bind(null, Discord, client));
-        }
-    }
+            try {
+                const event = require(`../events/${dirs}/${file}`);
+                const event_name = file.split('.')[0];
 
-    ['client', 'guild'].forEach(e => load_dir(e));
-}
+                if (event.once) {
+                    client.once(event_name, (...args) => event.execute(Discord, client, ...args));
+                } else {
+                    client.on(event_name, (...args) => event.execute(Discord, client, ...args));
+                }
+            } catch (error) {
+                console.error(`Error loading event file "${file}" in "${dirs}":`, error);
+            }
+        }
+    };
+
+    // Dynamically load all subdirectories in the 'events' folder
+    const directories = fs.readdirSync('./events').filter(dir => fs.lstatSync(`./events/${dir}`).isDirectory());
+    directories.forEach(load_dir);
+};
