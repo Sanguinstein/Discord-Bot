4d34da9e62639f6f482e860be7d610af11df4272
Sanguinstein <sangieeths@gmail.com>
2025-03-29 11:03:57 +0530

    Fixed Talk command (#13)
    Co-authored-by: SangeethS123 <71600115+SangeethS123@users.noreply.github.com>

--- Commands/BotCommands/talk.js
+++ Commands/BotCommands/talk.js
@@ -1,13 +1,15 @@
-const message = require("../events/guild/message")
-const { execute } = require("./sus")
-
 module.exports = {
-    name:'talk',
-    execute(message, args, Discord){
-        if(message.author.id !=='756245122324234448') return message.reply('Only bot maker can do that');
+    name: 'talk',
+    execute(message, args) {
+        if (message.author.id !== '756245122324234448') {
+            return message.reply('Only bot maker can do that');
+        }
+
         let speak = args.join(' ');
-         message.channel.send(speak).then(()=>{
-            message.delete();
-        }).catch(error => message.reply (error));
+        if (!speak) return message.reply("You didn't provide anything for me to say!");
+
+        message.channel.send(speak)
+            .then(() => message.delete())
+            .catch(error => message.reply(`Error: ${error.message}`));
     }
-}
+};
