ab1fd8f8b3421ab93e094ca51fcf23fade85aa18
Sanguinstein <sangieeths@gmail.com>
2025-03-31 23:47:18 +0530

    Mute and Unmute have been updated (#14)
    Mute and Unmute Modified

--- Commands/BotCommands/unmute.js
+++ Commands/BotCommands/unmute.js
@@ -1,28 +1,53 @@
-const { Channel } = require("discord.js")
+const ms = require('ms');
+const { EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
-    name: 'unmute',
-    description: "for unmuting",
-    execute(message, args, Discord, client){
-        if(message.member.permissions.has("BAN_MEMBERS")){
-            const user = message.mentions.users.first();
-            if(user){
-                let mainRole = message.guild.roles.cache.find(role => role.name === 'Verified' || role.name === 'Member');
-                let muteRole = message.guild.roles.cache.find(role => role.name === 'muted');  
-                
-                let membertarget = message.guild.members.cache.get(user.id);
-                var eeeembed = new Discord.MessageEmbed().setTitle('Notice').setColor('#92A8D1')
-                .setDescription(`<@${membertarget.user.id}> has been unmuted`);
-                
-                membertarget.roles.add(mainRole.id).catch(error =>client.users.cache.get('756245122324234448').send(`Sorry <${message.author}> I couldn't send the direct message because of : ${error}`));
-                membertarget.roles.remove(muteRole.id).catch(error => client.users.cache.get('756245122324234448').send(`Sorry <${message.author}> I couldn't send the direct message because of : ${error}`));
-                message.channel.send(eeeembed);
-                
-            }else{
-                message.reply(`Can't find that member`);
-            }
-        }else{
-            message.channel.send('You dont have the permission to execute this command');
+    name: "unmute",
+    description: "Unmutes a member by removing the 'Prisoner' role.",
+    execute(message, args, Discord, client) {
+        if (!message.member.permissions.has(PermissionFlagsBits.BanMembers)) {
+            return message.channel.send("? You don't have permission to execute this command.");
         }
-    }
-}
+
+        const user = message.mentions.users.first();
+        if (!user) return message.reply("You need to mention a user to unmute.");
+
+        let mainRole = message.guild.roles.cache.find(role => role.name === "Verified" || role.name === "Member");
+        let muteRole = message.guild.roles.cache.find(role => role.name === "Prisoner");
+
+        if (!muteRole) return message.channel.send("The 'Prisoner' role does not exist.");
+
+        let memberTarget = message.guild.members.cache.get(user.id);
+        if (!memberTarget) return message.reply("Could not find that member in this server.");
+
+        const { EmbedBuilder } = require('discord.js');
+
+        const unmuteEmbed = new EmbedBuilder()
+            .setTitle("\uD83D\uDD0A **User Unmuted**") // ?? Unicode Escape
+            .setColor('#00ff00')
+            .setDescription(`**<@${memberTarget.user.id}> has been unmuted by <@${message.author.id}>**`)
+            .addFields(
+                { name: "\uD83D\uDC6E Moderator", value: `<@${message.author.id}>`, inline: true } // ?? Unicode Escape
+            )
+            .setTimestamp();
+
+
+        // Remove 'Prisoner' (Mute) role and add back the main role
+        memberTarget.roles.remove(muteRole.id).catch(error =>
+            client.users.cache.get("756245122324234448").send(`Code error: ${error}`)
+        );
+
+        if (mainRole) {
+            memberTarget.roles.add(mainRole.id).catch(error =>
+                client.users.cache.get("756245122324234448").send(`Code error: ${error}`)
+            );
+        }
+
+        message.channel.send({ embeds: [unmuteEmbed] });
+
+        const logChannel = message.guild.channels.cache.find(
+            channel => channel.name === "reports" || channel.name === "logs"
+        );
+        if (logChannel) logChannel.send({ embeds: [unmuteEmbed] });
+    },
+};
