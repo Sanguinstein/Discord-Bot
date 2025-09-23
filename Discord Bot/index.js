a8fdd9b3c3099eba49ba1353ccee61bbdaeade7f
SangeethS123 <71600115+SangeethS123@users.noreply.github.com>
2025-09-23 20:59:54 +0530

    Webbrowser

--- Discord Bot/index.js
+++ Discord Bot/index.js
@@ -2,8 +2,19 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
 const EventHandler = require('./handlers/EventHandler');
+const express = require("express");
 
 dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot is running!");
+});
+
+app.listen(3000, () => {
+    console.log("? Web server is running on port 3000");
+});
 
 const client = new Client({
     intents: [
